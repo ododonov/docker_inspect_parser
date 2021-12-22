8faa57bd1bc89ea29f8b53583660ee5e314c7f64
ododonov <oleg_dodonov@mail.ru>
2021-12-22 10:03:01 +0000

    added installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo '# custom DIP function
+dip() {
+        local script_loc='$PWD' ;
+        sh -c "docker inspect /"$1/"> $script_loc/template.json" ;
+        python3 $script_loc/main.py ;
+        rm $script_loc/template.json ;
+}
+
+export -f dip' >> ~/.bashrc
+source ~/.bashrc
