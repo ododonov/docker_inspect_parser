a773b20606ed2d3ff7b5ba4f340f568d0c3dc0c5
ododonov <oleg_dodonov@mail.ru>
2021-12-22 12:00:11 +0000

    fixed bug

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@ echo '# custom DIP function
 dip() {
         sh -c "docker inspect /"$1/"> $DIP_HOME/template.json" ;
         python3 $DIP_HOME/main.py ;
-        rm $script_loc/template.json ;
+        rm $DIP_HOME/template.json ;
 }
 export $DIP_HOME='$PWD'
-export -f dip' >> ~/.bashrc;
-source ~/.bashrc;
+export -f dip' >> ~/.bashrc && source ~/.bashrc
