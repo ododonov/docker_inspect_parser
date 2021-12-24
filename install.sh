393e899523369ec71626ce624ab417e264dc478d
ododonov <oleg_dodonov@mail.ru>
2021-12-24 04:54:56 +0000

    fix

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ dip() {
         export DIP_RESULT=$(docker inspect /"$1/")
         python3 $DIP_HOME/main.py ;
 }
-export $DIP_HOME='$PWD'
+export DIP_HOME='$PWD'
 export -f dip' >> ~/.bashrc && source ~/.bashrc
