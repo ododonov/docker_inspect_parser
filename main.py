501e42b5834814ceb5f4146659862bcb056f23c6
ododonov <o.dodonov@avtomatika.kz>
2021-12-24 12:04:51 +0600

    added attributes to read

--- main.py
+++ main.py
@@ -7,34 +7,47 @@ os.chdir(path)
 json_file = (os.environ['DIP_RESULT'])
 data = json.loads(json_file)[0]
 
-#General info about container
-create_date = data["Created"]
+## General info about container
 container_state = data["State"]
 container_status = container_state["Status"]
+create_date = data["Created"]
+container_name = data["Name"]
 
+## Host config
+host_config = data["HostConfig"]
+container_image = host_config["Config"]["image"]
+
+#Volumes and ports
+binds = host_config["Binds"]
+ports = host_config["PortBindings"]
+
+## Container config
+container_config = data["Config"]
+envs = container_config["Env"]
+
+## Print information
 if(container_state["Running"]):
     container_status_color = '\033[92m'
 elif(container_state["Paused"]):
     container_status_color = '\033[91m'
 elif(container_state["Restarting"]):
     container_status_color = '\033[93m'
+else:
+    container_status_color = '\033[0m'
 print('-----')
+print('Name:', container_name)
+print('Image', container_image)
 print('Created at', create_date)
 print('Status:', container_status_color + container_status, '\033[0m')
 print('-----')
-
-#Volumes and ports
-host_config = data["HostConfig"]
-
 print('Volumes:')
-binds = host_config["Binds"]
 for i in range(len(binds)):
     print('- ', binds[i])
 print('Ports:')
-ports = host_config["PortBindings"]
-#print(host_config["PortBindings"]["3000/tcp"][0]["HostPort"])
-
 for i in ports:
     container_port = i
     host_port = ports[i][0]["HostIp"] + ':' + ports[i][0]["HostPort"]
     print('- ', container_port, ' : ', host_port)
+print('Environment:')
+for i in range(len(envs)):
+    print('- ', envs[i])
