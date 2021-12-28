ce97ed0eb79b0a398f9b00f000eee89002a995a2
ododonov <o.dodonov@avtomatika.kz>
2021-12-28 15:26:26 +0600

    added text style class

---
+++ str_style.py
@@ -0,0 +1,10 @@
+class str_style:
+    purple = '\033[95m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    gray = '\033[0m'
+    white = '\033[1m'
+    underlined = '\033[4m'
